822a72f533a9b4e038bc077d469573792a763cf2
Ezekiel <zexuding@outlook.com>
2024-07-23 02:23:54 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Dotfiles
+
+I use stow to manage and sync my dotfiles.
+
+## Installation
+
+```shell
+brew install stow
+
+git clone https://github.com/Dzx1025/dotfiles.git
+
+cd dotfiles
+```
+
+### Neovim
+
+```shell
+stow neovim
+```
---
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,165 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.g.lualine_laststatus = vim.o.laststatus
+    if vim.fn.argc(-1) > 0 then
+      -- set an empty statusline till lualine loads
+      vim.o.statusline = " "
+    else
+      -- hide the statusline on the starter page
+      vim.o.laststatus = 0
+    end
+  end,
+  opts = function()
+    -- PERF: we don't need this lualine require madness 🤷
+    local lualine_require = require("lualine_require")
+    lualine_require.require = require
+
+    local icons = LazyVim.config.icons
+
+    vim.o.laststatus = vim.g.lualine_laststatus
+
+    local opts = {
+      options = {
+        theme = "auto",
+        globalstatus = vim.o.laststatus == 3,
+        disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter" } },
+        section_separators = { left = "" },
+        component_separators = { left = "" },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = {
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+        },
+        lualine_c = {
+          {
+            "buffers",
+
+            show_filename_only = true, -- Shows shortened relative path when set to false.
+            hide_filename_extension = false, -- Hide filename extension when set to true.
+            show_modified_status = true, -- Shows indicator when the buffer is modified.
+
+            mode = 2, -- 0: Shows buffer name
+            -- 1: Shows buffer index
+            -- 2: Shows buffer name + buffer index
+            -- 3: Shows buffer number
+            -- 4: Shows buffer name + buffer number
+
+            max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
+            -- it can also be a function that returns the value of `max_length` dynamically.
+
+            filetype_names = {
+              TelescopePrompt = "Telescope",
+              dashboard = "Dashboard",
+              packer = "Packer",
+              fzf = "FZF",
+              alpha = "Alpha",
+            }, -- Shows specific buffer name for that filetype ( { `filetype` = `buffer_name`, ... } )
+
+            -- Automatically updates active buffer color to match color of other components (will be overidden if buffers_color is set)
+            use_mode_colors = true,
+
+            symbols = {
+              modified = " ●", -- Text to show when the buffer is modified
+              alternate_file = "#", -- Text to show to identify the alternate file
+              directory = "", -- Text to show when the buffer is a directory
+            },
+          },
+          LazyVim.lualine.root_dir(),
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { LazyVim.lualine.pretty_path() },
+        },
+        lualine_x = {
+          -- stylua: ignore
+          {
+            function() return require("noice").api.status.command.get() end,
+            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+            color = function() return LazyVim.ui.fg("Statement") end,
+          },
+          -- stylua: ignore
+          {
+            function() return require("noice").api.status.mode.get() end,
+            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+            color = function() return LazyVim.ui.fg("Constant") end,
+          },
+          -- stylua: ignore
+          {
+            function() return "  " .. require("dap").status() end,
+            cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+            color = function() return LazyVim.ui.fg("Debug") end,
+          },
+          -- stylua: ignore
+          {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = function() return LazyVim.ui.fg("Special") end,
+          },
+          "branch",
+          {
+            "diff",
+            symbols = {
+              added = icons.git.added,
+              modified = icons.git.modified,
+              removed = icons.git.removed,
+            },
+            source = function()
+              local gitsigns = vim.b.gitsigns_status_dict
+              if gitsigns then
+                return {
+                  added = gitsigns.added,
+                  modified = gitsigns.changed,
+                  removed = gitsigns.removed,
+                }
+              end
+            end,
+          },
+        },
+        lualine_y = {
+          { "progress", separator = " ", padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
+        lualine_z = {
+          -- function()
+          --   return " " .. os.date("%R")
+          -- end,
+        },
+      },
+      extensions = { "neo-tree", "lazy" },
+    }
+
+    -- do not add trouble symbols if aerial is enabled
+    -- And allow it to be overriden for some buffer types (see autocmds)
+    if
+      vim.g.trouble_luline_enabed ~= false
+      and vim.b.trouble_lualine_enabled ~= false
+      and LazyVim.has("trouble.nvim")
+    then
+      local trouble = require("trouble")
+      local symbols = trouble.statusline
+        and trouble.statusline({
+          mode = "symbols",
+          groups = {},
+          title = false,
+          filter = { range = true },
+          format = "{kind_icon}{symbol.name:Normal}",
+          hl_group = "lualine_c_normal",
+        })
+      table.insert(opts.sections.lualine_c, {
+        symbols and symbols.get,
+        cond = symbols and symbols.has,
+      })
+    end
+
+    return opts
+  end,
+}
