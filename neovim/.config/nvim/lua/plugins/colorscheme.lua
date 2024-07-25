794e13dd815fcd15006a6766a2925f6c5dae6ca3
Ezekiel <zexuding@outlook.com>
2024-07-25 14:06:06 +0800

    enable auto dark and transparent bg

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,17 @@ return {
   { "rebelot/kanagawa.nvim" },
   { "ellisonleao/gruvbox.nvim" },
   {
+    "catppuccin/nvim",
+    flavour = "auto", -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+      light = "latte",
+      dark = "mocha",
+    },
+    opts = {
+      transparent_background = true,
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
