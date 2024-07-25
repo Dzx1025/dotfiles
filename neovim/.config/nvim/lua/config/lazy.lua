4fedc997926bdb46b3b8330cd73a72e716edf459
Ezekiel <zexuding@outlook.com>
2024-07-25 23:07:44 +0800

    Add degy.nvim plugin

--- neovim/.config/nvim/lua/config/lazy.lua
+++ neovim/.config/nvim/lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
+    { import = "lazyvim.plugins.extras.ui.edgy" },
 
     { import = "lazyvim.plugins.extras.editor.telescope" },
 
